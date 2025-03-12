78ad89776b9b1d3acae9d39c694659c94dc1f0fe
jimiyeh <jimiyeh913@gmail.com>
2025-03-12 18:55:23 +0800

    [refactor] 調整目錄結構

--- app/src/main/java/com/example/attractions/ui/MainActivity.kt
+++ app/src/main/java/com/example/attractions/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.attractions
+package com.example.attractions.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.attractions.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
