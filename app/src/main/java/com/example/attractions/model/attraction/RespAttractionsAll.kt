0609150f7f8b90c8650f8d2552786e6c45631b97
jimiyeh <jimiyeh913@gmail.com>
2025-03-12 19:04:30 +0800

    [feat] 新增最新消息以及旅遊景點 model

---
+++ app/src/main/java/com/example/attractions/model/attraction/RespAttractionsAll.kt
@@ -0,0 +1,86 @@
+package com.example.attractions.model.attraction
+
+
+import com.google.gson.annotations.SerializedName
+
+data class RespAttractionsAll(
+    @SerializedName("data")
+    val data: List<Data> = listOf(),
+    @SerializedName("total")
+    val total: Int = 0
+) {
+    data class Data(
+        @SerializedName("address")
+        val address: String = "",
+        @SerializedName("category")
+        val category: List<Category> = listOf(),
+        @SerializedName("distric")
+        val distric: String = "",
+        @SerializedName("elong")
+        val elong: Double = 0.0,
+        @SerializedName("email")
+        val email: String = "",
+        @SerializedName("facebook")
+        val facebook: String = "",
+        @SerializedName("fax")
+        val fax: String = "",
+        @SerializedName("files")
+        val files: List<Any> = listOf(),
+        @SerializedName("friendly")
+        val friendly: List<Any> = listOf(),
+        @SerializedName("id")
+        val id: Int = 0,
+        @SerializedName("images")
+        val images: List<Any> = listOf(),
+        @SerializedName("introduction")
+        val introduction: String = "",
+        @SerializedName("links")
+        val links: List<Any> = listOf(),
+        @SerializedName("modified")
+        val modified: String = "",
+        @SerializedName("months")
+        val months: String = "",
+        @SerializedName("name")
+        val name: String = "",
+        @SerializedName("name_zh")
+        val nameZh: Any = Any(),
+        @SerializedName("nlat")
+        val nlat: Double = 0.0,
+        @SerializedName("official_site")
+        val officialSite: String = "",
+        @SerializedName("open_status")
+        val openStatus: Int = 0,
+        @SerializedName("open_time")
+        val openTime: String = "",
+        @SerializedName("remind")
+        val remind: String = "",
+        @SerializedName("service")
+        val service: List<Any> = listOf(),
+        @SerializedName("staytime")
+        val staytime: String = "",
+        @SerializedName("target")
+        val target: List<Target> = listOf(),
+        @SerializedName("tel")
+        val tel: String = "",
+        @SerializedName("ticket")
+        val ticket: String = "",
+        @SerializedName("url")
+        val url: String = "",
+        @SerializedName("zipcode")
+        val zipcode: String = ""
+    ) {
+        data class Category(
+            @SerializedName("id")
+            val id: Int = 0,
+            @SerializedName("name")
+            val name: String = ""
+        )
+
+        data class Target(
+            @SerializedName("id")
+            val id: Int = 0,
+            @SerializedName("name")
+            val name: String = ""
+        )
+    }
+}
---
+++ app/src/main/java/com/example/attractions/model/event/RespEventsNews.kt
@@ -0,0 +1,41 @@
+package com.example.attractions.model.event
+
+
+import com.google.gson.annotations.SerializedName
+
+data class RespEventsNews(
+    @SerializedName("data")
+    val `data`: List<Data> = listOf(),
+    @SerializedName("total")
+    val total: Int = 0
+) {
+    data class Data(
+        @SerializedName("begin")
+        val begin: Any = Any(),
+        @SerializedName("description")
+        val description: String = "",
+        @SerializedName("end")
+        val end: Any = Any(),
+        @SerializedName("files")
+        val files: List<Any> = listOf(),
+        @SerializedName("id")
+        val id: Int = 0,
+        @SerializedName("links")
+        val links: List<Link> = listOf(),
+        @SerializedName("modified")
+        val modified: String = "",
+        @SerializedName("posted")
+        val posted: String = "",
+        @SerializedName("title")
+        val title: String = "",
+        @SerializedName("url")
+        val url: String = ""
+    ) {
+        data class Link(
+            @SerializedName("src")
+            val src: String = "",
+            @SerializedName("subject")
+            val subject: String = ""
+        )
+    }
+}
